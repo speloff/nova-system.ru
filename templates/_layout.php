9fa818a2882614bda023476ae2a2763e0430d955
Ivan Slepov <speloff@gmail.com>
2012-08-01 16:16:20 +0400

    tables

--- templates/_layout.php
+++ templates/_layout.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Таблицы</title>
+	<title>Tables</title>
 
 	<!--jQuery-->
 	<script src="/js/jquery-1.7.2.min.js"></script>
